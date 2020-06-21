52d68813a1b6c6537ccdca53621c918895ca1562
ni_n_ja <rampage4extrm@gmail.com>
2020-06-21 18:13:54 +0900

    first commit

---
+++ web.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+    var video = document.querySelector("#videoElement");
+
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                video.srcObject = stream;
+            })
+            .catch(function (err) {
+                console.log("ERROR: ", err);
+            });
+    }
+};
