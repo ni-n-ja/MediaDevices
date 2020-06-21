a889182ce501ec2237037617359ee3f9fdd8a169
ni-n-ja <rampage4extrm@gmail.com>
2020-06-21 19:23:00 +0900

    Update web.js
    💎

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 window.onload = () => {
-    var video = document.querySelector("#videoElement");
+    let video = document.querySelector("#videoElement");
 
     if (navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: { width: 1920, height: 1080 } })
-            .then(function (stream) {
+            .then((stream) => {
                 video.srcObject = stream;
             })
-            .catch(function (err) {
+            .catch((err) => {
                 console.log("ERROR: ", err);
             });
     }
